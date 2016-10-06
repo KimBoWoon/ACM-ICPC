033e2f9e0f3710ceddbafeeb1316a955717f238f
김보운 <haioer@naver.com>
2016-10-06 18:18:40 +0900

    prob 2493 challenge

---
+++ 2493/2493.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <stack>
+
+using namespace std;
+
+#pragma warning(disable:4996)
+
+int main() {
+	int n, temp;
+	int *arr;
+	stack<int> s;
+
+	scanf("%d", &n);
+
+	while (!s.empty()) {
+		
+	}
+
+	return 0;
+}
