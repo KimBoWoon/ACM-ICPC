8f5d7d6c6b5835b40c53a08dc67d7ce036c7c2c5
김보운 <haioer@naver.com>
2016-10-06 18:49:28 +0900

    prob 2493 challenge

--- 2493/2493.cpp
+++ 2493/2493.cpp
@@ -6,14 +6,24 @@ using namespace std;
 #pragma warning(disable:4996)
 
 int main() {
-	int n, temp;
-	int *arr;
+	int n = 0, height = 0;
+	int *arr = NULL;
 	stack<int> s;
 
 	scanf("%d", &n);
 
-	while (!s.empty()) {
-		
+	while (true) {
+		int temp;
+
+		scanf("%d", &temp);
+		s.push(temp);
+
+		if (height > s.top()) {
+			printf("%d ", 1);
+		}
+		else {
+			printf("0 ");
+		}
 	}
 
 	return 0;
