d047a5a8e4a666eb6b451486120e9d1c796906ee
KimBoWoon <haioer@naver.com>
2016-10-09 23:07:36 +0900

    Prob 1012 added bfs serach

--- 1012/1012.cpp
+++ 1012/1012.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstring>
+#include <queue>
 using namespace std;
 
 #pragma warning(disable:4996)
@@ -7,6 +8,7 @@ using namespace std;
 int arr[51][51];
 int dx[] = { 1, -1, 0, 0 }, dy[] = { 0, 0, 1, -1 };
 int testCase, row, col, n, cnt;
+queue< pair<int, int> > q;
 
 void dfs(int x, int y) {
 	if (x < 0 || y < 0 || x > row || y > col || !arr[x][y]) {
@@ -17,6 +19,17 @@ void dfs(int x, int y) {
 
 	for (int i = 0; i < 4; i++) {
 		dfs(x + dx[i], y + dy[i]);
+	}
+}
+
+void bfs(int x, int y) {
+	arr[x][y] = 0;
+	q.pop();
+	for (int i = 0; i < 4; i++) {
+		if (arr[x + dx[i]][y + dy[i]] == 1) {
+			arr[x + dx[i]][y + dy[i]] = 0;
+			q.push(make_pair(x + dx[i], y + dy[i]));
+		}
 	}
 }
 
@@ -44,6 +57,18 @@ int main() {
 				}
 			}
 		}
+
+		/*for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (arr[i][j]) {
+					cnt++;
+					q.push(make_pair(i, j));
+					while (q.size()) {
+						bfs(q.front().first, q.front().second);
+					}
+				}
+			}
+		}*/
 
 		printf("%d\n", cnt);
 	}
