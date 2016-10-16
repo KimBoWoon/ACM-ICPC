617e912669d66d800200c7845fbbaff02dae21a2
KimBoWoon <haioer@naver.com>
2016-10-17 00:16:15 +0900

    Prob 2294 Solving

---
+++ 2294/2294.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+#pragma warning(disable:4996)
+
+int coin[101] = { 0 }, cache[10001] = { 0 };
+
+int main() {
+	int n = 0, k = 0;
+
+	scanf("%d %d", &n, &k);
+
+	memset(cache, -1, sizeof(int) * 10001);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &coin[i]);
+	}
+
+	cache[0] = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = coin[i]; j <= k; j++) {
+			if (j >= coin[i] && cache[j - coin[i]] != -1) {
+				if (cache[j] == -1) {
+					cache[j] = cache[j - coin[i]] + 1;
+				}
+				else {
+					cache[j] = min(cache[j], cache[j - coin[i]] + 1);
+				}
+			}
+		}
+	}
+
+	printf("%d\n", cache[k]);
+}
