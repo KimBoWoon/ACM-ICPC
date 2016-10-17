de52c81e35dee39584ea5bf93547bcbf23a6d6ff
김보운 <haioer@naver.com>
2016-10-17 14:44:55 +0900

    Prob 10171 Solving

---
+++ 10171/10171.cpp
@@ -0,0 +1,15 @@
+//
+// Created by BoWoon on 2016-10-17.
+//
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+
+    return 0;
+}
