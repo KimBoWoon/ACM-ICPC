bfab67741c14ff75e67201c598886ca170068293
KimBoWoon <haioer@naver.com>
2016-10-26 00:01:58 +0900

    Prob 10844 Solving

--- 10844/10844.cpp
+++ 10844/10844.cpp
@@ -3,30 +3,31 @@ using namespace std;
 
 #pragma warning(disable:4996)
 
-int arr[101][101];
+#define MOD 1000000000
+
+unsigned long long arr[101][11];
 
 int main() {
 	int n;
 
 	scanf("%d", &n);
 
-	arr[0][1] = 0;
-	for (int i = 1; i <= 9; i++) {
-		arr[i][1] = 1;
+	for (int i = 2; i <= 10; i++) {
+		arr[0][i] = 1;
 	}
 
 	for (int i = 1; i < n; i++) {
-		for (int j = 1; j <= 9; j++) {
-			arr[j][i] = arr[j - 1][i - 1] + arr[j + 1][i - 1];
+		for (int j = 1; j <= 10; j++) {
+			arr[i][j] = (arr[i - 1][j - 1] + arr[i - 1][j + 1]) % MOD;
 		}
 	}
 
-	for (int i = 1; i < n; i++) {
-		for (int j = 1; j <= 9; j++) {
-			printf("%d ", arr[j][i]);
-		}
-		printf("\n");
+	unsigned long long sum = 0;
+	for (int i = 1; i <= 10; i++) {
+		sum = (sum + arr[n - 1][i]) % MOD;
 	}
+
+	printf("%lld\n", sum % MOD);
 
 	return 0;
 }
