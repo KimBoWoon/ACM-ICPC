e6e7743eae93dddabb808bd9bd2776d961c5f047
김보운 <haioer@naver.com>
2016-10-28 17:35:15 +0900

    Prob 11048 Solving

---
+++ 1620/1620.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#pragma warning(disable:4996)
+
+int n, m;
+vector<string> v;
+
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		v.push_back(s);
+	}
+
+	for (int i = 0; i < m; i++) {
+		
+	}
+}
