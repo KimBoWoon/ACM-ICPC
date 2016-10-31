f06e17bc2a66c3fa15249aca7fa61b5bdcb73b59
KimBoWoon <haioer@naver.com>
2016-11-01 00:11:06 +0900

    Prob 13415 Solving

--- 13415/13415.cpp
+++ 13415/13415.cpp
@@ -1,34 +1,60 @@
 #include <cstdio>
 #include <algorithm>
 #include <functional>
+#include <vector>
 using namespace std;
 
 #pragma warning(disable:4996)
 
+typedef struct order {
+	int num, flag;
+} Order;
+
 int n, k;
-int arr1[100001], arr2[2][100001];
+int arr[100001];
+vector<Order> v;
 
 int main() {
 	scanf("%d", &n);
 
 	for (int i = 1; i <= n; i++) {
-		scanf("%d", &arr1[i]);
+		scanf("%d", &arr[i]);
 	}
 
 	scanf("%d", &k);
 
-	for (int i = 0; i < k; i++) {
-		scanf("%d %d", &arr2[0][i], &arr2[1][i]);
+	for (int i = 0; i < k * 2; i++) {
+		int x;
+		scanf("%d", &x);
+		
+		while (true) {
+			if (v.empty()) {
+				v.push_back({ x, i % 2 });
+				break;
+			}
+			else {
+				if (v[v.size() - 1].num < x) {
+					v.pop_back();
+				}
+				else {
+					v.push_back({ x, i % 2 });
+					break;
+				}
+			}
+		}
 	}
 
-	int maxA = arr2[0][k - 1], maxD = arr2[1][k - 1];
-	for (int i = k - 1; i; i--) {
-		sort(arr1 + 1, arr1 + arr2[0][i] + 1, less<int>());
-		sort(arr1 + 1, arr1 + arr2[1][i] + 1, greater<int>());
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i].flag) {
+			sort(arr + 1, arr + v[i].num + 1, greater<int>());
+		}
+		else {
+			sort(arr + 1, arr + v[i].num + 1, less<int>());
+		}
 	}
 
 	for (int i = 1; i <= n; i++) {
-		printf("%d ", arr1[i]);
+		printf("%d ", arr[i]);
 	}
 	printf("\n");
 
