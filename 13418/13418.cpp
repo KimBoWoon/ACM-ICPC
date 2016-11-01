c9a10f534af442f64ab46f6ed9bea432b4bb9b64
김보운 <haioer@naver.com>
2016-11-01 18:58:06 +0900

    Prob 13418 Challenge

---
+++ 13418/13418.cpp
@@ -0,0 +1,109 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#pragma warning(disable:4996)
+
+typedef struct edge {
+	int start, end, weight;
+} Edge;
+
+int up[1001];
+int weight[1001];
+int n, m, x, y, t, minCost = 0, maxCost = 0;
+vector<Edge> edge, selectEdge;
+
+bool comp1(const Edge x, const Edge y) {
+	return x.weight < y.weight;
+}
+
+bool comp2(const Edge x, const Edge y) {
+	return x.weight > y.weight;
+}
+
+int SimpleFind(const int i) {
+	int root = i;
+
+	while (up[root] > 0)
+		root = up[root];
+
+	return root;
+}
+
+void Union(const int i, const int j) {
+	int rooti = SimpleFind(i);
+	int rootj = SimpleFind(j);
+
+	if (weight[rooti] > weight[rootj]) {
+		up[rootj] = rooti;
+		weight[rooti] = weight[rooti] + weight[rootj];
+		weight[rootj] = 0;
+	}
+	else if (weight[rooti] < weight[rootj]) {
+		up[rooti] = rootj;
+		weight[rootj] = weight[rooti] + weight[rootj];
+		weight[rooti] = 0;
+	}
+	else if (weight[rooti] == weight[rootj]) {
+		if (rooti > rootj) {
+			up[rootj] = rooti;
+			weight[rooti] = weight[rooti] + weight[rootj];
+			weight[rootj] = 0;
+		}
+		else {
+			up[rooti] = rootj;
+			weight[rootj] = weight[rooti] + weight[rootj];
+			weight[rooti] = 0;
+		}
+	}
+}
+
+void Kruskal() {
+	int edgeCnt = 0;
+
+	while (selectEdge.size() < n) {
+		int x = edge[edgeCnt].start, y = edge[edgeCnt].end;
+		int p = SimpleFind(x);
+		int q = SimpleFind(y);
+
+		if (q != p) {
+			Union(q, p);
+			selectEdge.push_back({ edge[edgeCnt].start, edge[edgeCnt].end, edge[edgeCnt].weight });
+		}
+		edgeCnt++;
+	}
+}
+
+int main() {
+	bool(*comp[2])(Edge, Edge) = { comp1, comp2 };
+	int cost[2] = { 0 };
+
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < m + 1; i++) {
+		scanf("%d %d %d", &x, &y, &t);
+		edge.push_back({ x, y, t });
+	}
+
+	for (int i = 0; i < 2; i++) {
+		sort(edge.begin(), edge.end(), comp[i]);
+
+		for (int i = 0; i < 1000; i++) {
+			up[i] = 0;
+			weight[i] = 1;
+		}
+
+		Kruskal();
+
+		for (int j = 0; j < selectEdge.size(); j++) {
+			cost[i] += selectEdge[j].weight;
+		}
+
+		selectEdge.clear();
+	}
+
+	printf("%d\n", (cost[1] * cost[1]) - (cost[0] * cost[0]));
+
+	return 0;
+}
