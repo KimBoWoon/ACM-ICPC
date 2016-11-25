a468c24812cee1881bf6960353067fb67e0b21a2
김보운 <haioer@naver.com>
2016-11-25 15:22:18 +0900

    Prob  1357 Solving

---
+++ 1357/1357.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string x, y, sum;
+
+	cin >> x >> y;
+
+	reverse(x.begin(), x.end());
+	reverse(y.begin(), y.end());
+
+	sum = to_string(stoi(x) + stoi(y));
+
+	reverse(sum.begin(), sum.end());
+
+	cout << sum << endl;
+}
