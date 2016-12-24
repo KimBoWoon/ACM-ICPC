9789202277994e0898f91aacbc0440eca2fbba4a
KimBoWoon <haioer@naver.com>
2016-12-24 17:56:56 +0900

    update

--- 4963/4963.cpp
+++ 4963/4963.cpp
@@ -8,24 +8,16 @@ int map[51][51], visited[51][51];
 int dx[9] = { 0, 1, 1, 1, 0, -1, -1, -1 };
 int dy[9] = { -1, -1, 0, 1, 1, 1, 0, -1 };
 
-void dfs(int y, int x, int ans) {
-	/*visited[i][j] = ans;
+void dfs(int i, int j, int ans) {
+	visited[i][j] = ans;
 
 	for (int t = 0; t < 9; t++) {
-		if (1 <= i + dx[t] && i + dx[t] <= w && 1 <= j + dy[t] && j + dy[t] <= h) {
-			if (map[i + dx[t]][j + dy[t]] == 1 && visited[i + dx[t]][j + dy[t]] == 0) {
-				dfs(i + dx[t], j + dy[t], ans);
-			}
-		}
-	}*/
-	visited[y][x] = ans;
-
-	for (int i = 0; i<9; i++) {
-		int tx = x + dx[i];
-		int ty = y + dy[i];
+		int tx = j + dx[t];
+		int ty = i + dy[t];
 		if (1 <= tx && tx <= w && 1 <= ty && ty <= h) {
-			if (map[ty][tx] == 1 && visited[ty][tx] == 0)
+			if (map[ty][tx] == 1 && visited[ty][tx] == 0) {
 				dfs(ty, tx, ans);
+			}
 		}
 	}
 }
@@ -40,14 +32,15 @@ int main() {
 			break;
 		}
 
-		for (int i = 1; i <= w; i++) {
-			for (int j = 1; j <= h; j++) {
+		for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= w; j++) {
 				scanf("%d", &map[i][j]);
+				visited[i][j] = 0;
 			}
 		}
 
-		for (int i = 1; i <= w; i++) {
-			for (int j = 1; j <= h; j++) {
+		for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= w; j++) {
 				if (visited[i][j] == 0 && map[i][j] == 1) {
 					dfs(i, j, ++ans);
 				}
