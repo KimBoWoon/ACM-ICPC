8b075b0a59d337e42009440d1feb3b39d2ff09f3
김보운 <haioer@naver.com>
2017-04-20 11:06:09 +0900

    1427 solving

---
+++ 1427/1427.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+
+	cin >> s;
+
+	sort(s.begin(), s.end(), greater<char>());
+
+	cout << s << endl;
+}
