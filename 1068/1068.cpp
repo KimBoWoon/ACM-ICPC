207fbae0b92efa36183ea0e162355ebde2f5d3e9
김보운 <haioer@naver.com>
2017-04-27 15:01:22 +0900

    reload

--- 1068/1068.cpp
+++ 1068/1068.cpp
@@ -1,55 +1,40 @@
 #include <cstdio>
-using namespace std;
 
-int n;
-int parent[55];
-int node[55], check[55];
+int n, r, d, par[55], cnt;
 
-int erase(int no) {
-	if (no == -1) {
-		return 0;
-	}
-	if (check[no] == 1) {
-		return 1;
+void dfs(int ix) {
+	int i = n, fl = 1;
+
+	if (ix == d) {
+		return;
 	}
 
-	return check[no] = erase(parent[no]);
-}
-
-int solve(int rem) {
-	int i;
-	int cnt = 0;
-
-	check[rem] = 1;
-	node[parent[rem]]--;
-
-	for (i = 0; i < n; i++) {
-		if (node[i] == 0) {
-			if (erase(i) == 0) {
-				cnt++;
-			}
+	while (i--) {
+		if (par[i] == ix && i != d) {
+			dfs(i), fl = 0;
 		}
 	}
 
-	return cnt;
+	cnt += fl;
 }
 
 int main() {
-	int i, p;
-	int rem;
+	int i;
 
 	scanf("%d", &n);
 
 	for (i = 0; i < n; i++) {
-		scanf("%d", &p);
-		parent[i] = p;
-		if (p != -1) {
-			node[p]++;
+		scanf("%d", &par[i]);
+		if (par[i] < 0) {
+			r = i;
 		}
 	}
-	scanf("%d", &rem);
 
-	printf("%d\n", solve(rem));
+	scanf("%d", &d);
+
+	dfs(r);
+
+	printf("%d", cnt);
 
 	return 0;
 }
