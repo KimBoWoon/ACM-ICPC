80e3c90c9350421298ececfeb92ae1d2261ae77b
KimBoWoon <haioer@naver.com>
2017-05-04 15:36:12 +0900

    1520 solving

--- 1520/1520.cpp
+++ 1520/1520.cpp
@@ -1,46 +1,40 @@
-//
-// Created by secret on 1/4/17.
-//
-
 #include <cstdio>
 using namespace std;
 
-#pragma warning(disable:4996)
-
-int map[501][501];
-int d[2] = { 1, -1 };
+int map[501][501], ans[501][501];
+int dx[4] = { 0, 1, 0, -1 }, dy[4] = { -1, 0, 1, 0 };
 int m, n;
 
-int dp(int x, int y)
-{
-    int ans = 0;
-	int tX, tY;
+int dp(int x, int y) {
+	if (x < 0 || y < 0 || x >= m || y >= n) {
+		return 0;
+	}
+	if (ans[x][y] >= 0) {
+		return ans[x][y];
+	}
+	int ret = 0;
+	for (int i = 0; i < 4; i++) {
+		if (map[x][y] > map[x + dx[i]][y + dy[i]]) {
+			ret += dp(x + dx[i], y + dy[i]);
+		}
+	}
 
-    if (x == m && y == n)
-        return 1;
-
-    for (int i = 0; i < 2; i++)
-	{
-		tX = x + d[i];
-		tY = y + d[i];
-
-		if (tX <= m && tX >= 1 && map[tX][y] < map[x][y])
-			ans += dp(x + d[i], y);
-
-		if (tY <= n && tY >= 1 && map[x][tY] < map[x][y])
-			ans += dp(x, y + d[i]);
-    }
-
-    return ans;
+	return ans[x][y] = ret;
 }
 
-int main()
-{
-    scanf("%d %d", &m, &n);
+int main() {
+	scanf("%d %d", &m, &n);
 
-    for (int i = 1; i <= m; i++)
-        for (int j = 1; j <= n; j++)
-            scanf("%d", &map[i][j]);
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &map[i][j]);
+			ans[i][j] = -1;
+		}
+	}
 
-    printf("%d\n", dp(1, 1));
+	ans[m - 1][n - 1] = 1;
+
+	printf("%d\n", dp(0, 0));
+
+	return 0;
 }
