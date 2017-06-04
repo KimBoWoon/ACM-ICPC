d05ebf94a3320bf1eec18b48225e21623c205f02
KimBoWoon <haioer@naver.com>
2017-06-04 17:59:48 +0900

    5585 solving

---
+++ 5585/5585.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+using namespace std;
+
+int n, sum, ans;
+int coin[] = { 500, 100, 50, 10, 5, 1 };
+
+int main() {
+	scanf("%d", &n);
+
+	sum = 1000 - n;
+
+	for (int i = 0; sum != 0;) {
+		if (sum >= coin[i]) {
+			sum -= coin[i];
+			ans++;
+		}
+		else {
+			i++;
+		}
+	}
+
+	printf("%d\n", ans);
+}
