5da1c6eb604d0261f25bf0c821bebdf2e2a89f3a
김보운 <haioer@naver.com>
2017-06-08 16:46:39 +0900

    2309 solving

---
+++ ACM-ICPC/2309.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int a[10], sum = 0;
+
+int main() {
+	for (int i = 0; i < 9; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	sort(a, a + 9);
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9 && j != i; j++) {
+			sum = 0;
+			for (int k = 0; k < 9; k++) {
+				if (k != i && k != j) {
+					sum += a[k];
+				}
+			}
+			if (sum == 100) {
+				for (int k = 0; k < 9; k++) {
+					if (k != i && k != j) {
+						printf("%d\n", a[k]);
+					}
+				}
+			}
+		}
+	}
+}
