65ff19eb2341f1b661a6b5fbe17c66df96986240
KimBoWoon <haioer@naver.com>
2017-06-26 15:59:06 +0900

    segment tree challenge

---
+++ ACM-ICPC/10999.cpp
@@ -0,0 +1,99 @@
+//
+// Created by secret on 6/25/17.
+//
+
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+#define MAX_SIZE 1000001
+
+typedef long long LLONG;
+
+int n, m, k, a, b, c, d;
+LLONG tree[MAX_SIZE], lazy[MAX_SIZE], arr[MAX_SIZE];
+
+LLONG init(int node, int start, int end) {
+    if (start == end) {
+        return tree[node] = arr[start];
+    } else {
+        return tree[node] = init(node * 2, start, (start + end) / 2) + init(node * 2 + 1, (start + end) / 2 + 1, end);
+    }
+}
+
+void update_lazy(int node, int start, int end) {
+    if (lazy[node] != 0) {
+        tree[node] += (end - start + 1) * lazy[node];
+
+        if (start != end) {
+            lazy[node * 2] += lazy[node];
+            lazy[node * 2 + 1] += lazy[node];
+        }
+
+        lazy[node] = 0;
+    }
+}
+
+void update_range(int node, int start, int end, int left, int right, LLONG diff) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return;
+    }
+
+    if (left <= start && end <= right) {
+        tree[node] += (end - start + 1) * diff;
+        if (start != end) {
+            lazy[node * 2] += diff;
+            lazy[node * 2 + 1] += diff;
+        }
+        return;
+    }
+
+    update_range(node * 2, start, (start + end) / 2, left, right, diff);
+    update_range(node * 2 + 1, (start + end) / 2 + 1, end, left, right, diff);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+LLONG sum(int node, int start, int end, int left, int right) {
+    update_lazy(node, start, end);
+    if (left > end || right < start) {
+        return 0;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+    return sum(node * 2, start, (start + end) / 2, left, right) +
+           sum(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+}
+
+int main() {
+    scanf("%d %d %d", &n, &m, &k);
+
+//    int h = (int) ceil(log2(n));
+//    int tree_size = (1 << (h + 1)) - 1;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &arr[i]);
+    }
+
+    init(1, 0, n - 1);
+
+    for (int i = 0; i < m + k; i++) {
+        scanf("%d", &a);
+
+        if (a == 1) {
+            int start, end;
+            long long v;
+
+            scanf("%d %d %lld", &start, &end, &v);
+
+            update_range(1, 0, n - 1, start - 1, end - 1, v);
+        } else if (a == 2) {
+            int start, end;
+
+            scanf("%d %d", &start, &end);
+            printf("%lld\n", sum(1, 0, n - 1, start - 1, end - 1));
+        }
+    }
+    return 0;
+}
--- ACM-ICPC/2042.cpp
+++ ACM-ICPC/2042.cpp
@@ -2,47 +2,133 @@
 // Created by secret on 6/24/17.
 //
 
-#include<stdio.h>
+//#include<stdio.h>
+//
+//typedef unsigned long long ULLONG;
+//ULLONG bit[1000001], a[1000001];
+//int n, m, k;
+//ULLONG x, y, z;
+//
+//void update(int h, ULLONG x) {
+//    for (int i = h; i <= n; i += i & -i) {
+//        bit[i] += x;
+//    }
+//}
+//
+//ULLONG query(int h) {
+//    ULLONG r = 0;
+//
+//    for (int i = h; i; i -= i & -i) {
+//        r += bit[i];
+//    }
+//
+//    return r;
+//}
+//
+//int main() {
+//    scanf("%d %d %d", &n, &m, &k);
+//
+//    for (int i = 1; i <= n; i++) {
+//        scanf("%lld", &a[i]);
+//        update(i, a[i]);
+//    }
+//
+//    for (int i = 0; i < m + k; i++) {
+//        scanf("%lld %lld %lld", &x, &y, &z);
+//
+//        if (x == 1) {
+//            update(y, z - a[y]);
+//            a[y] = z;
+//        } else {
+//            printf("%lld\n", query(z) - query(y - 1));
+//        }
+//    }
+//
+//    return 0;
+//}
 
-typedef unsigned long long ULLONG;
-ULLONG bit[1000001], a[1000001];
-int n, m, k;
-ULLONG x, y, z;
+#include <cstdio>
+#include <cmath>
 
-void update(int h, ULLONG x) {
-    for (int i = h; i <= n; i += i & -i) {
-        bit[i] += x;
+typedef long long LLONG;
+int n, m, k, h, tree_size;
+LLONG tree[1000010], arr[1000010];
+
+LLONG init(int node, int start, int end) {
+    if (start == end) {
+        return tree[node] = arr[start];
+    }
+
+    int mid = (start + end) / 2;
+
+    return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
+}
+
+void update(int node, int start, int end, int index, LLONG diff) {
+    if (!(start <= index && index <= end)) {
+        return;
+    }
+
+    tree[node] += diff;
+
+    if (start != end) {
+        int mid = (start + end) / 2;
+
+        update(node * 2, start, mid, index, diff);
+        update(node * 2 + 1, mid + 1, end, index, diff);
     }
 }
 
-ULLONG query(int h) {
-    ULLONG r = 0;
-
-    for (int i = h; i; i -= i & -i) {
-        r += bit[i];
+LLONG sum(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) {
+        return 0;
     }
 
-    return r;
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+
+    int mid = (start + end) / 2;
+
+    return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
 }
 
 int main() {
     scanf("%d %d %d", &n, &m, &k);
 
-    for (int i = 1; i <= n; i++) {
-        scanf("%lld", &a[i]);
-        update(i, a[i]);
+//    h = (int) ceil(log2(n));
+//    tree_size = (1 << (h + 1));
+
+    for (int i = 0; i < n; i++) {
+        scanf("%lld", &arr[i]);
     }
+
+    init(1, 0, n - 1);
 
     for (int i = 0; i < m + k; i++) {
-        scanf("%lld %lld %lld", &x, &y, &z);
+        int a;
 
-        if (x == 1) {
-            update(y, z - a[y]);
-            a[y] = z;
-        } else {
-            printf("%lld\n", query(z) - query(y - 1));
+        scanf("%d", &a);
+
+        if (a == 1) {
+            int b;
+            LLONG c;
+            scanf("%d %lld", &b, &c);
+            update(1, 0, n - 1, b - 1, c - arr[b - 1]);
+            arr[b - 1] = c;
+        } else if (a == 2) {
+            int b, c;
+            scanf("%d %d", &b, &c);
+            printf("%lld\n", sum(1, 0, n - 1, b - 1, c - 1));
         }
-    }
 
-    return 0;
+//        for (int k = 0; k < n; k++) {
+//            printf("%lld ", arr[k]);
+//        }
+//        printf("\n");
+//        for (int k = 1; k <= tree_size; k++) {
+//            printf("%lld ", tree[k]);
+//        }
+//        printf("\n");
+    }
 }
