e3d17c46abfe2eee01325b3d064510b081118ea7
김보운 <haioer@naver.com>
2017-06-28 15:11:38 +0900

    6359 solving

---
+++ ACM-ICPC/6359.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+
+int main() {
+	int t, n;
+
+	scanf("%d", &t);
+
+	while (t--) {
+		int d[101];
+
+		scanf("%d", &n);
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; i*j <= n; j++) {
+				if (d[i*j]) {
+					d[i*j] = !d[i*j];
+				}
+				else {
+					d[i*j] = !d[i*j];
+				}
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			d[0] += d[i];
+		}
+		printf("%d\n", d[0]);
+	}
+}
+
+int main() {
+	int i, t, n;
+
+	scanf("%d", &t);
+
+	while (t--) {
+		scanf("%d", &n);
+
+		for (i = 1; i * i <= n; i++);
+
+		printf("%d\n", i - 1);
+	}
+}
