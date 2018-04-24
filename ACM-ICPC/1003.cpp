60831b0656400d823dafd9a6b3629762a064da47
KimBoWoon <haioer@naver.com>
2018-04-24 16:01:18 +0900

    update problem 1003

--- ACM-ICPC/1003.cpp
+++ ACM-ICPC/1003.cpp
@@ -1,41 +1,37 @@
-#include <iostream>
+#include <cstdio>
 using namespace std;
 
-int cnt0 = 0, cnt1 = 0;
+int arr[41] = {1, 1};
 
-int fibonacci(int n);
-
-int main()
-{
-	int cases;
-
-	cin >> cases;
-
-	for (int i = 0; i < cases; i++)
-	{
-		int n = 0;
-
-		cin >> n;
-
-		fibonacci(n);
-
-		cout << cnt0 << " " << cnt1 << endl;
-
-		cnt0 = cnt1 = 0;
-	}
+int fibonacci(int n) {
+    if (n <= 1) {
+        return arr[n];
+    } else {
+        if (arr[n] > 0) {
+                return arr[n];
+        }
+    }
+    
+    return arr[n] = fibonacci(n - 1) + fibonacci(n - 2);
 }
-int fibonacci(int n)
-{
-	if (n == 0)
-	{
-		cnt0++;
-		return 0;
-	}
-	else if (n == 1)
-	{
-		cnt1++;
-		return 1;
-	}
-	else
-		return fibonacci(n - 1) + fibonacci(n - 2);
+
+int main() {
+    int testCase;
+    
+    scanf("%d", &testCase);
+    
+    while (testCase--) {
+        int n;
+        
+        scanf("%d", &n);
+        
+        if (n == 0) {
+            printf("1 0\n");
+        } else if (n == 1) {
+            printf("0 1\n");
+        } else {
+            fibonacci(n);
+            printf("%d %d\n", arr[n - 2], arr[n - 1]);
+        }
+    }
 }
