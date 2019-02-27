27d16f212681e4155ce7e00931d34bcfa4bd6236
KimBoWoon <haioer@naver.com>
2019-02-27 15:46:52 +0900

    7569

--- ACM-ICPC/7569.cpp
+++ ACM-ICPC/7569.cpp
@@ -12,6 +12,18 @@ int dx[] = { 0, 1, 0, -1, 0, 0 }, dy[] = { 1, 0, -1, 0, 0, 0 }, dz[] = { 0, 0, 0
 int map[101][101][101], visit[101][101][101];
 queue<point> q;
 
+// void printMap() {
+// 	for (int i = 0; i < h; i++) {
+// 		for (int j = 0; j < m; j++) {
+//             for (int k = 0; k < n; k++) {
+//                 printf("%d ", map[i][j][k]);
+//             }
+//             printf("\n");
+// 		}
+//         printf("\n");
+// 	}
+// }
+
 void bfs() {
 	while (!q.empty()) {
 		int px = q.front().x, py = q.front().y, pz = q.front().z;
@@ -20,12 +32,12 @@ void bfs() {
 		for (int i = 0; i < 6; i++) {
 			int nx = px + dx[i];
 			int ny = py + dy[i];
-            int nz = pz + dz[i];
+			int nz = pz + dz[i];
 
-			if (nx > -1 && nx < h && ny > -1 && ny < n && nz > -1 && nz < m) {
-				if (visit[nx][ny][nz] == 0) {
+			if (nx > -1 && nx < h && ny > -1 && ny < m && nz > -1 && nz < n) {
+				if (map[nx][ny][nz] == 0) {
 					q.push({ nx, ny, nz });
-					visit[nx][ny][nz] = visit[px][py][nz] + 1;
+					map[nx][ny][nz] = map[px][py][pz] + 1;
 				}
 			}
 		}
@@ -34,13 +46,13 @@ void bfs() {
 
 bool check() {
 	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
-			    if (visit[i][j][k] == 0) {
+		for (int j = 0; j < m; j++) {
+            for (int k = 0; k < n; k++) {
+			    if (map[i][j][k] == 0) {
     				return false;
 			    }
 			    else {
-				    ans = max(ans, visit[i][j][k]);
+				    ans = max(ans, map[i][j][k]);
 			    }
 		    }
         }
@@ -50,42 +62,20 @@ bool check() {
 }
 
 int main() {
-	scanf("%d %d %d", &m, &n, &h);
+	scanf("%d %d %d", &n, &m, &h);
 
 	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
+		for (int j = 0; j < m; j++) {
+            for (int k = 0; k < n; k++) {
                 scanf("%d", &map[i][j][k]);
-                visit[i][j][k] = map[i][j][k];
                 if (map[i][j][k] == 1) {
                     q.push({ i, j, k });
-                    visit[i][j][k] = 1;
                 }
             }
 		}
 	}
 
-	// for (int i = 0; i < h; i++) {
-	// 	for (int j = 0; j < m; j++) {
-    //         for (int k = 0; k < n; k++) {
-    //             printf("%d ", map[i][j][k]);
-    //         }
-    //         printf("\n");
-	// 	}
-    //     printf("\n");
-	// }
-
 	bfs();
-
-    for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n; j++) {
-            for (int k = 0; k < m; k++) {
-                printf("%d ", visit[i][j][k]);
-            }
-            printf("\n");
-		}
-        printf("\n");
-	}
 
 	if (!check()) {
 		printf("-1\n");
@@ -94,89 +84,3 @@ int main() {
 		printf("%d\n", ans - 1);
 	}
 }
-
-// #include <stdio.h>
-// #include <queue>
-// using namespace std;
- 
-// int m, n, h, cnt;
-// int tomato[100][100][100];
-// int visit[100][100][100];
-// int dir[6][3] = { {-1,0,0},{1,0,0},{0,-1,0},{0,1,0},{0,0,-1},{0,0,1} };
- 
-// struct col {
-//     int x, y, z;
-// };
-// queue<col> q;
- 
-// int bfs() {
-//     int ans = 1;
-//     while (!q.empty()) {
-//         int size = q.size();
-//         for (int i = 0; i < size; ++i) {
-//             col c1 = q.front();
-//             q.pop();
-//             int x = c1.x, y = c1.y, z = c1.z;
- 
-//             if (visit[x][y][z])
-//                 continue;
-//             else
-//                 visit[x][y][z] = 1;
- 
-//             for (int j = 0; j < 6; ++j) {
-//                 int xx = x + dir[j][0];
-//                 int yy = y + dir[j][1];
-//                 int zz = z + dir[j][2];
- 
-//                 if (xx < 0 || xx >= h)
-//                     continue;
-//                 if (yy < 0 || yy >= n)
-//                     continue;
-//                 if (zz < 0 || zz >= m)
-//                     continue;
-//                 if (tomato[xx][yy][zz] == -1)
-//                     continue;
- 
-//                 if (tomato[xx][yy][zz] == 0) {
-//                     q.push({ xx,yy,zz });
-//                     tomato[xx][yy][zz] = tomato[x][y][z] + 1;
-//                     cnt--;
-//                 }
-                
-//                 if (cnt == 0)
-//                     return ans;
-//             }
-//         }
-//         ans++;
-//     }
-//     return -1;
-// }
- 
-// int main() {
-//     scanf("%d %d %d", &m, &n, &h);
-//     for (int i = 0; i < h; ++i) {
-//         for (int j = 0; j < n; ++j) {
-//             for (int k = 0; k < m; ++k) {
-//                 scanf("%d", &tomato[i][j][k]);
-//                 if (tomato[i][j][k] == 0)
-//                     cnt++;
-//                 else if (tomato[i][j][k] == 1)
-//                     q.push({ i,j,k });
-//             }
-//         }
-//     }
-
-//     printf("%d\n", bfs());
-
-//     for (int i = 0; i < h; i++) {
-// 		for (int j = 0; j < n; j++) {
-//             for (int k = 0; k < m; k++) {
-//                 printf("%d ", tomato[i][j][k]);
-//             }
-//             printf("\n");
-// 		}
-//         printf("\n");
-// 	}
- 
-//     return 0;
-// }
