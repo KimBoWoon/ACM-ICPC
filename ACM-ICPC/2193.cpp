149e912fb0c849470ca508d46087f39f36d088e2
KimBoWoon <haioer@naver.com>
2019-05-10 18:21:32 +0900

    2193

--- ACM-ICPC/2193.cpp
+++ ACM-ICPC/2193.cpp
@@ -1,26 +1,39 @@
-//
-// Created by secret on 10/11/16.
-//
+#include <bits/stdc++.h>
 
-#include <cstdio>
-using namespace std;
+typedef unsigned long long ULLONG;
 
-#pragma warning(disable:4996)
+int n;
+ULLONG dp[100];
 
-unsigned long long arr[100];
-
-int main() {
-    int n;
-
-    scanf("%d", &n);
-
-    arr[0] = 0;
-    arr[1] = 1;
-    for (int i = 2; i <= n; i++) {
-        arr[i] = arr[i - 1] + arr[i - 2];
+ULLONG topDown(int x) {
+    if (x == 0) {
+        return dp[0] = 0;
+    }
+    if (x == 1) {
+        return dp[1] = 1;
     }
 
-    printf("%lld\n", arr[n]);
+    if (dp[x] != 0) {
+        return dp[x];
+    }
+
+    return dp[x] = topDown(x - 1) + topDown(x - 2);
+}
+
+ULLONG bottomUp(int x) {
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+}
+
+int main() {
+    scanf("%d", &n);
+
+    memset(dp, 0, sizeof(dp));
+    printf("%lld\n", topDown(n));
+    // printf("%lld\n", bottomUp(n));
 
     return 0;
 }
