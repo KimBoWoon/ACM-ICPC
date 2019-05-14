71335fa1852d8e070a67710a6f86d23847208991
KimBoWoon <haioer@naver.com>
2019-05-14 17:06:51 +0900

    11052

--- ACM-ICPC/11052.cpp
+++ ACM-ICPC/11052.cpp
@@ -1,27 +1,46 @@
-#include <cstdio>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-#pragma warning(disable:4996)
-
 int n;
-int arr[10001], d[10001];
+int card[10001], dp[10001];
+
+int topDown(int x) {
+	if (x == 0) {
+		return 0;
+	}
+
+	if (dp[x] != -1) {
+		return dp[x];
+	}
+
+	for (int i = 1; i <= x; i++) {
+		dp[x] = max(dp[x], topDown(x - i) + card[i]);
+	}
+
+	return dp[x];
+}
+
+int bottomUp(int x) {
+	dp[1] = card[1];
+	for (int i = 1; i <= x; i++) {
+		for (int j = 1; j <= i; j++) {
+			dp[i] = max(dp[i], dp[i - j] + card[j]);
+		}
+	}
+
+	return dp[x];
+}
 
 int main() {
 	scanf("%d", &n);
 
 	for (int i = 1; i <= n; i++) {
-		scanf("%d", &arr[i]);
+		scanf("%d", &card[i]);
 	}
 
-	d[1] = arr[1];
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			d[i] = max(d[i], d[i - j] + arr[j]);
-		}
-	}
-
-	printf("%d\n", d[n]);
+	// memset(dp, -1, sizeof(dp));
+	// printf("%d\n", topDown(n));
+	printf("%d\n", bottomUp(n));
 
 	return 0;
 }
