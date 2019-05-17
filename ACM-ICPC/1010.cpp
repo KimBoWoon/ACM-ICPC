118e90c85ec831839c5605a191f227efd848d8d6
KimBoWoon <haioer@naver.com>
2019-05-17 14:56:24 +0900

    1010

--- ACM-ICPC/1010.cpp
+++ ACM-ICPC/1010.cpp
@@ -1,18 +1,29 @@
-#include<cstdio>
-using namespace std;
+#include <bits/stdc++.h>
 
-#pragma warning(disable:4996)
+typedef unsigned long long ULLONG;
 
-int testcase, left, right;
-int ans = 0;
-unsigned long long dp[31][31];
+int t, left, right;
+ULLONG dp[31][31];
 
-void fill_dp(int l, int r) {
+ULLONG topDown(int l, int r) {
+	if (dp[l][r] != 0) {
+		return dp[l][r];
+	}
+	if (l == r || r == 0) {
+		return 1;
+	} else if (l < r) {
+		return 0;
+	} else {
+		return dp[l][r] = topDown(l - 1, r) + topDown(l - 1, r - 1);
+	}
+}
+
+void bottomUp(int l, int r) {
 	for (int i = 0; i < l; i++) {
 		dp[i][0] = 1;
 	}
-	for (int j = 0; j < r; j++) {
-		dp[j][j] = 1;
+	for (int i = 0; i < r; i++) {
+		dp[i][i] = 1;
 	}
 	for (int i = 1; i < l; i++) {
 		for (int j = 1; j < r; j++) {
@@ -20,15 +31,18 @@ void fill_dp(int l, int r) {
 		}
 	}
 }
-int main() {
-	fill_dp(31, 31);
-	
-	scanf("%d", &testcase);
 
-	for (int i = 0; i < testcase; i++) {
-		ans = 0;
+int main() {
+	memset(dp, 0, sizeof(dp));
+	topDown(31, 31);
+	// bottomUp(31, 31);
+	
+	scanf("%d", &t);
+
+	for (int i = 0; i < t; i++) {
 		scanf("%d %d", &left, &right);
-		printf("%lld\n", dp[right][left]);
+		printf("%lld\n", topDown(right, left));
+		// printf("%lld\n", dp[right][left]);
 	}
 	return 0;
 }
