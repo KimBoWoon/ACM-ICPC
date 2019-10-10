5af90a98413cc1cc63623f8ccca76b15903d3a17
KimBoWoon <haioer@naver.com>
2019-10-10 21:52:43 +0900

    1328

--- ACM-ICPC/1328.cpp
+++ ACM-ICPC/1328.cpp
@@ -21,7 +21,7 @@ int main() {
     for (int i = 2; i <= n; i++) {
         for (int j = 1; j <= l; j++) {
             for (int k = 1; k <= r; k++) {
-                dp[i][j][k] = dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] + dp[i - 1][j][k] * (n - 2);
+                dp[i][j][k] = dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] + dp[i - 1][j][k] * (i - 2);
                 dp[i][j][k] %= 1000000007;
             }
         }
