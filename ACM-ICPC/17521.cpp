4bc76bad55ef6e35c27cb39c08cd09efccf6ae34
KimBoWoon <haioer@naver.com>
2019-10-22 19:54:04 +0900

    17521

---
+++ ACM-ICPC/17521.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+using namespace std;
+
+long long int n, w, coin, low, hi;
+int graph[16];
+bool flag = false;
+
+int main() {
+    scanf("%lld %lld", &n, &w);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &graph[i]);
+    }
+
+    low = graph[0];
+
+    for (int i = 1; i < n; i++) {
+        if (low < graph[i]) {
+            hi = graph[i];
+            ++i;
+            for (i; hi < graph[i] && i < n; i++) {
+                hi = graph[i];
+            }
+            w = (w / low) * hi + w % low;
+        }
+        low = graph[i];
+    }
+
+    printf("%lld\n", w);
+
+    // low = graph[0];
+    // for (int i = 1; i < n; i++) {
+    //     if (low > graph[i]) {
+    //         low = graph[i];
+    //     } else if (low < graph[i] && !flag) {
+    //         coin = w / low;
+    //         w %= low;
+    //         hi = graph[i];
+    //         flag = true;
+    //     } else if (hi < graph[i]) {
+    //         hi = graph[i];
+    //     } else if (hi > graph[i] && flag) {
+    //         w += coin * hi;
+    //         coin = 0;
+    //         low = graph[i];
+    //         flag = false;
+    //     }
+    // }
+
+    // printf("%lld\n", coin * graph[n - 1] + w);
+}
