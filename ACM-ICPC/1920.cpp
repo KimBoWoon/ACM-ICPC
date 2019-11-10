97e1be71b0ac1226ecc10ed0683fcd5b1d8ea974
KimBoWoon <haioer@naver.com>
2019-11-10 19:01:40 +0900

    modify 1920

--- ACM-ICPC/1920.cpp
+++ ACM-ICPC/1920.cpp
@@ -1,40 +1,68 @@
-#include <cstdlib>
 #include <cstdio>
-#include <vector>
-#include <algorithm>
-
+#include <cstdlib>
 using namespace std;
 
-int main() {
-	int n, m;
-	int *q;
+int n, m, q;
+int arr[100001];
 
-	scanf("%d", &n);
+int comp(const void* x, const void* y) {
+	if (*(int*)x < *(int*)y) {
+		return -1;
+	} else if (*(int*)x > *(int*)y) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
-	q = (int *)malloc(n * sizeof(int));
+bool rbinary(int s, int e, int target) {
+	int mid = (s + e) / 2;
 
-	for (int i = 0; i < n; ++i) {
-		scanf("%d", &q[i]);
+	while (s <= e) {
+		if (arr[mid] == target) {
+			return true;
+		} else if (arr[mid] > target) {
+			return rbinary(s, mid - 1, target);
+		} else if (arr[mid] < target) {
+			return rbinary(mid + 1, e, target);
+		}
 	}
 
-	sort(q, q + n);
+	return false;
+}
+
+bool binary(int s, int e, int target) {
+	while (s <= e) {
+		int mid = (s + e) / 2;
+
+		if (arr[mid] == target) {
+			return true;
+		} else if (arr[mid] > target) {
+			e = mid - 1;
+		} else if (arr[mid] < target) {
+			s = mid + 1;
+		}
+	}
+
+	return false;
+}
+
+int main() {
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	// sort(q, q + n);
+	qsort(arr, n, sizeof(int), comp);
 
 	scanf("%d", &m);
 
-	for (int i = 0; i < m; ++i) {
-		int ans;
-		//            vector<int>::iterator iter;
+	for (int i = 0; i < m; i++) {
+		scanf("%d", &q);
 
-		scanf("%d", &ans);
-
-		//            iter = find(q.begin(), q.end(), ans);
-		//
-		//            if(iter != q.end())
-		//                printf("1\n");
-		//            else
-		//                printf("0\n");
-
-		if (binary_search(q, q + n, ans))
+		if (binary(0, n - 1, q))
 			printf("1\n");
 		else
 			printf("0\n");
