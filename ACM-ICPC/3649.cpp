d2552f4111c58a23ec3b584e9c611c2901117164
KimBoWoon <haioer@naver.com>
2019-11-29 23:30:27 +0900

    3649

--- ACM-ICPC/3649.cpp
+++ ACM-ICPC/3649.cpp
@@ -1,19 +1,41 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <algorithm>
+#include <memory.h>
+using namespace std;
 
 int x, n;
 int lego[1000001];
 
-void binarySearch() {
-    // 하나의 값을 선택하고 필요한 길이에서 뺀 뒤 나머길이를 이분 탐색 한다
-    for (int i = 0; i < n - 1; i++) {
-        int cur = lego[i];
+bool rBinarySearch(int start, int end, int target) {
+    while (start <= end) {
+        int mid = (start + end) / 2;
 
-        if (std::binary_search(lego + i + 1, lego + n, x - cur)) {
-            printf("yes %d %d\n", cur, x - cur);
-            return;
+        if (lego[mid] == target) {
+            return true;
+        } else if (lego[mid] > target) {
+            return rBinarySearch(start, mid - 1, target);
+        } else if (lego[mid] < target) {
+            return rBinarySearch(mid + 1, end, target);
         }
     }
-    printf("danger\n");
+
+    return false;
+}
+
+bool binarySearch(int start, int end, int target) {
+    while (start <= end) {
+        int mid = (start + end) / 2;
+
+        if (lego[mid] == target) {
+            return true;
+        } else if (lego[mid] > target) {
+            end = mid - 1;
+        } else if (lego[mid] < target) {
+            start = mid + 1;
+        }
+    }
+
+    return false;
 }
 
 int main() {
@@ -26,8 +48,22 @@ int main() {
             scanf("%d", &lego[i]);
         }
 
-        std::sort(lego, lego + n);
+        sort(lego, lego + n);
 
-        binarySearch();
+        // 하나의 값을 선택하고 필요한 길이에서 뺀 뒤 나머지길이를 이분 탐색 한다
+        bool flag = false;
+        for (int i = 0; i < n - 1; i++) {
+            int cur = lego[i];
+
+            if (binarySearch(i + 1, n, x - cur)) {
+                printf("yes %d %d\n", cur, x - cur);
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            printf("danger\n");
+        }
+        memset(lego, 0, sizeof(int) * 1000001);
     }
 }
