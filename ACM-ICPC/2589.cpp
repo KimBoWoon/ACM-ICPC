e32f30a7c56cba6ec743e614ca9fc976c2150407
KimBoWoon <haioer@naver.com>
2020-03-04 20:42:37 +0900

    2589

--- ACM-ICPC/2589.cpp
+++ ACM-ICPC/2589.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 #include <queue>
-#include <memory.h>
+#include <algorithm>
 using namespace std;
 
 typedef struct Point {
@@ -8,15 +8,14 @@ typedef struct Point {
 } point;
 
 char map[51][51];
-int x, y, answer, dist;
-int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
-int visit[51][51];
-queue<point> q;
+int l, w, answer;
+int dx[] = { 0, 1, 0, -1 }, dy[] = { 1, 0, -1, 0 };
+int visited[51][51];
 
-void printVisit() {
-	for (int i = 0; i < x; i++) {
-		for (int j = 0; j < y; j++) {
-            printf("%d ", visit[i][j]);
+void printvisited() {
+	for (int i = 0; i < l; i++) {
+		for (int j = 0; j < w; j++) {
+            printf("%d ", visited[i][j]);
 		}
         printf("\n");
 	}
@@ -24,71 +23,75 @@ void printVisit() {
 }
 
 void init() {
-    dist = 0;
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
+    for (int i = 0; i < l; i++) {
+        for (int j = 0; j < w; j++) {
             if (map[i][j] == 'L') {
-                visit[i][j] = 1;
+                visited[i][j] = 0;
             } else {
-                visit[i][j] = -1;
+                visited[i][j] = -1;
             }
         }
     }
 }
 
-void bfs() {
+void bfs(int x, int y) {
+    queue<point> q;
+    q.push({ x, y });
+    visited[x][y] = 0;
+
     while (!q.empty()) {
-        int px = q.front().x, py = q.front().y;
+        point p = q.front();
         q.pop();
 
         for (int i = 0; i < 4; i++) {
-            int nx = px + dx[i];
-            int ny = py + dy[i];
+            int nx = p.x + dx[i], ny = p.y + dy[i];
 
-            if (nx > -1 && nx < x && ny > -1 && ny < y) {
-                if (visit[nx][ny] == 1) {
+            if (nx >= 0 && nx < l && ny >= 0 && ny < w) {
+                if (map[nx][ny] == 'W' || (nx == x && ny == y)) {
+                    continue;
+                }
+                if (visited[nx][ny] == 0 || visited[nx][ny] > visited[p.x][p.y] + 1) {
                     q.push({ nx, ny });
-                    visit[nx][ny] = visit[px][py] + 1;
+                    visited[nx][ny] = visited[p.x][p.y] + 1;
                 }
             }
         }
     }
 }
 
-void check() {
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
-            if (visit[i][j] > dist) {
-                dist = visit[i][j];
+int check() {
+    int dist = 0;
+
+    for (int i = 0; i < l; i++) {
+        for (int j = 0; j < w; j++) {
+            if (visited[i][j] > dist) {
+                dist = visited[i][j];
             }
         }
     }
+
+    return dist;
 }
 
 int main() {
-    scanf("%d %d", &x, &y);
+    scanf("%d %d", &l, &w);
 
-    for (int i = 0; i < x; i++) {
+    for (int i = 0; i < l; i++) {
         scanf("%s", map[i]);
     }
 
     // 시작점은 모든 육지
     // 제일 큰 값을 가져오면 된다
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
+    for (int i = 0; i < l; i++) {
+        for (int j = 0; j < w; j++) {
             if (map[i][j] == 'L') {
-                q.push({ i, j });
-                visit[i][j] = 1;
-                bfs();
-                check();
-                // printVisit();
-                if (answer < dist) {
-                    answer = dist;
-                }
                 init();
+                bfs(i, j);
+                // printvisited();
+                answer = max(answer, check());
             }
         }
     }
 
-    printf("%d\n", answer - 1);
+    printf("%d\n", answer);
 }
