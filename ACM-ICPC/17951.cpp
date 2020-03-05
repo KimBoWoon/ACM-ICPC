622abb42afd826b1dc9878674b1599c0418c78d7
KimBoWoon <haioer@naver.com>
2020-03-05 16:31:26 +0900

    17951

--- ACM-ICPC/17951.cpp
+++ ACM-ICPC/17951.cpp
@@ -1,30 +1,49 @@
 #include <cstdio>
-#include <cmath>
+#include <algorithm>
 using namespace std;
 
-int n, k, x, answer = 2e9;
+int n, k, high, answer = -1;
 int arr[100001];
+
+void parametric(int start, int end) {
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        int sum = 0, cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (sum >= mid) {
+                cnt++;
+                sum = 0;
+            }
+        }
+
+        // cnt가 k이상이 되면 k개의 그룹으로 나눌수 있다
+        // 그러므로 받을 수 있는 점수의 최대는 mid + 1이 된다
+        if (cnt >= k) {
+            start = mid + 1;
+            answer = mid;
+        } else {
+            // k개의 그룹으로 나눌수 없는 경우이므로
+            // end 점수는 받을 수 없다
+            // 그러므로 end 값을 mid - 1로 대체
+            end = mid - 1;
+        }
+    }
+
+    printf("%d\n", answer);
+}
 
 int main() {
     scanf("%d %d", &n, &k);
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
+        high += arr[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        
-    }
-    // for (int i = 1; i <= n; i++) {
-    //     scanf("%d", &x);
-    //     arr[i] = arr[i - 1] + x;
-    // }
-
-    // for (int i = 0; i < k; i++) {
-    //     if (answer > abs(arr[n / k * i] - arr[(n / k) * (i + 1)])) {
-    //         answer = abs(arr[n / k * i] - arr[(n / k) * (i + 1)]);
-    //     }
-    // }
-
-    // printf("%d\n", answer);
+    parametric(0, high);
 }
+
+// 파라메트릭 서치를 사용하면 풀리는 문제
