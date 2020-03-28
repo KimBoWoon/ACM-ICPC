6583ae7d3dd109a6e875050d6fd38102ae3556b0
KimBoWoon <haioer@naver.com>
2020-03-28 23:39:15 +0900

    add comment

--- ACM-ICPC/1012.cpp
+++ ACM-ICPC/1012.cpp
@@ -8,27 +8,37 @@ using namespace std;
 int arr[51][51];
 int dx[] = { 1, -1, 0, 0 }, dy[] = { 0, 0, 1, -1 };
 int testCase, row, col, n, cnt;
-queue< pair<int, int> > q;
 
 void dfs(int x, int y) {
-	if (x < 0 || y < 0 || x > row || y > col || !arr[x][y]) {
+	if (x < 0 || y < 0 || x > row || y > col || !arr[x][y]) { // 범위안에 없거나 배추가 있는곳이 아니면 리턴
 		return;
 	}
 
-	arr[x][y] = 0;
+	arr[x][y] = 0; // 확인하고 배추가 있던 곳을 0으로 체크
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++) { // 다음 탐색지 체크
 		dfs(x + dx[i], y + dy[i]);
 	}
 }
 
 void bfs(int x, int y) {
+	queue< pair<int, int> > q;
+	q.push({ x, y });
 	arr[x][y] = 0;
-	q.pop();
-	for (int i = 0; i < 4; i++) {
-		if (arr[x + dx[i]][y + dy[i]] == 1) {
-			arr[x + dx[i]][y + dy[i]] = 0;
-			q.push(make_pair(x + dx[i], y + dy[i]));
+	
+	while (!q.empty()) {
+		pair<int, int> value = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = value.first + dx[i], ny = value.second + dy[i];
+
+			if (nx > -1 && nx < row && ny > -1 && ny < col) { // 다음 좌표가 배추밭 안에 있다면
+				if (arr[value.first + dx[i]][value.second + dy[i]] == 1) {
+					arr[value.first + dx[i]][value.second + dy[i]] = 0; // 확인하고 배추가 있던 곳을 0으로 체크
+					q.push(make_pair(value.first + dx[i], value.second + dy[i])); // 큐에 다음 탐색지 저장
+				}
+			}
 		}
 	}
 }
@@ -37,7 +47,7 @@ int main() {
 	scanf("%d", &testCase);
 
 	while (testCase--) {
-		memset(arr, 0, 51 * 51);
+		memset(arr, 0, sizeof(arr));
 
 		cnt = 0;
 
@@ -49,26 +59,23 @@ int main() {
 			arr[x][y] = 1;
 		}
 
+		// for (int i = 0; i < row; i++) {
+		// 	for (int j = 0; j < col; j++) {
+		// 		if (arr[i][j]) { // 배추가 있는 곳이면 dfs 탐색 시작
+		// 			cnt++;
+		// 			dfs(i, j);
+		// 		}
+		// 	}
+		// }
+
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
-				if (arr[i][j]) {
+				if (arr[i][j]) { // 배추가 있는 곳이면 bfs 탐색 시작
 					cnt++;
-					dfs(i, j);
+					bfs(i, j);
 				}
 			}
 		}
-
-		/*for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if (arr[i][j]) {
-					cnt++;
-					q.push(make_pair(i, j));
-					while (q.size()) {
-						bfs(q.front().first, q.front().second);
-					}
-				}
-			}
-		}*/
 
 		printf("%d\n", cnt);
 	}
--- ACM-ICPC/2667.cpp
+++ ACM-ICPC/2667.cpp
@@ -15,7 +15,7 @@ queue<point> q;
 vector<int> v;
 
 void bfs() {
-	homeCnt = 1;
+	homeCnt = 1; // 몇개의 집이 있는지 체크하는 변수
 
 	while (!q.empty()) {
 		int px = q.front().x, py = q.front().y;
@@ -25,8 +25,8 @@ void bfs() {
 			int nx = px + dx[i];
 			int ny = py + dy[i];
 
-			if (nx > -1 && nx < n && ny > -1 && ny < n) {
-				if (map[nx][ny] == 1) {
+			if (nx > -1 && nx < n && ny > -1 && ny < n) { // 범위 안에 있으며
+				if (map[nx][ny] == 1) { // 집이 있는곳 이면 다음 탐색지
 					q.push({ nx, ny });
 					map[nx][ny] = 0;
 					visit[nx][ny] = visit[px][py] + 1;
@@ -50,7 +50,7 @@ int main() {
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (map[i][j] == 1) {
+			if (map[i][j] == 1) { // 집이 있는곳 이면 bfs 탐색 시작
 				q.push({ i, j });
 				map[i][j] = 0;
 				visit[i][j] = 1;
