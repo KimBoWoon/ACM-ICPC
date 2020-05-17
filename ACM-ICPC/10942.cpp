70ce2ecd0df0c66d6eed7617194014d4d84b6546
KimBoWoon <haioer@naver.com>
2020-05-17 14:13:39 +0900

    add comment

--- ACM-ICPC/10942.cpp
+++ ACM-ICPC/10942.cpp
@@ -1,27 +1,53 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
 
 int s, e, n, m;
 int arr[2001];
-int dp[2001][2001];
+// int dp[2001][2001];
+bool dp[2001][2001];
 
-int check(int s, int e) {
-    if (s >= e) {
-        return 1;
+// int check(int s, int e) {
+//     if (s >= e) {
+//         return 1;
+//     }
+
+//     int& ret = dp[s][e];
+
+//     if (ret != -1) {
+//         return ret;
+//     }
+
+//     if (arr[s] != arr[e]) {
+//         return 0;
+//     }
+
+//     ret = check(s + 1, e - 1);
+
+//     return ret;
+// }
+
+void solution() {
+    //길이 1
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = true;
     }
 
-    int& ret = dp[s][e];
-
-    if (ret != -1) {
-        return ret;
+    //길이 2
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] == arr[i + 1]) {
+            dp[i][i + 1] = true;
+        }
     }
 
-    if (arr[s] != arr[e]) {
-        return 0;
+    //길이 3 이상
+    for (int i = 2; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            //앞 뒤가 같고, 앞 뒤 사이에 있는 수들이 팰린드롬인 경우
+            if (arr[j] == arr[j + i] && dp[j + 1][j + i - 1]) {
+                dp[j][j + i] = true;
+            }
+        }
     }
-
-    ret = check(s + 1, e - 1);
-
-    return ret;
 }
 
 int main() {
@@ -31,17 +57,25 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    memset(dp, -1, sizeof(dp));
-
     scanf("%d", &m);
+    // memset(dp, -1, sizeof(dp));
+    memset(dp, false, sizeof(dp));
+
+    solution();
 
     for (int i = 0; i < m; i++) {
         scanf("%d %d", &s, &e);
-        
-        if (check(s - 1, e - 1)) {
+
+        if (dp[s - 1][e - 1]) {
             printf("1\n");
         } else {
             printf("0\n");
         }
+        
+        // if (check(s - 1, e - 1)) {
+        //     printf("1\n");
+        // } else {
+        //     printf("0\n");
+        // }
     }
 }
